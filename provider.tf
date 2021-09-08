869a86d5a1f2cbe1644cea4fb3f3bf88b027c479
manishmf <manishmf.067@gmail.com>
2021-09-08 16:15:44 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIARK65IFHUP4DU3N3Q"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
